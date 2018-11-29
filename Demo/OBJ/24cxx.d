a13d6ae2876c9ddd141dec7db36580a7bf92e8e7
hello147 <33335876+hello147@users.noreply.github.com>
2018-11-29 21:02:30 +0800

    test

--- Demo/OBJ/24cxx.d
+++ Demo/OBJ/24cxx.d
@@ -3,6 +3,7 @@
 ..\obj\24cxx.o: ..\SYSTEM\iic\iic.h
 ..\obj\24cxx.o: ..\SYSTEM\sys\sys.h
 ..\obj\24cxx.o: ..\USER\stm32F10x.h
+..\obj\24cxx.o: ..\CORE\core_cm3.h
 ..\obj\24cxx.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 ..\obj\24cxx.o: ..\USER\system_stm32f10x.h
 ..\obj\24cxx.o: ..\USER\stm32f10x_conf.h
